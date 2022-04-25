e9f00d5347b7b61d23b7fbd41474623e9f02c1df
Abeldlp <abel45991690@gmail.com>
2022-04-25 10:42:24 +0200

    Added needed ui library and axios

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(Quasar, { plugins: {} });
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
